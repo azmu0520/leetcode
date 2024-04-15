53cd627097341de20786c553b3ab53cea7d27a92
azmu0520 <azizbekbootcam@gmail.com>
2024-04-15 20:32:38 +0200

    done

--- array/easy/test.js
+++ array/easy/test.js
@@ -1,6 +1,10 @@
-var merge = function (nums1, m, nums2, n) {
-  nums1.splice(m, nums1.length, ...nums2.splice(0, n));
-  nums1 = nums1.sort((a, b) => a - b);
-  return nums1;
+var generate = function (numRows) {
+  let res = [];
+  for (let i = 0; i < numRows; i++) {
+    let item = [];
+    for (let j = 0; j < i; j++) {}
+  }
+
+  return res;
 };
-console.log(merge([1, 2, 3, 0, 0, 0], 0, [2, 5, 6], 3));
+console.log(generate(5));
